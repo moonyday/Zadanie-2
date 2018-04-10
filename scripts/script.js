3d908a997037ae66b1a7eb2f1afa4a869057ff09
Marta <marta.poczatek@gmail.com>
2018-04-10 07:20:00 +0200

    mobile-nav fixed

--- scripts/script.js
+++ scripts/script.js
@@ -3,54 +3,56 @@
 	$(document).ready(function(){
 
 
-//hamburger (mobile-size)
-$(".hamburger").on("click", function(){
-	$("nav").slideToggle();
-})
-$(window).resize(function(){
-	if ($(window).width() > 580) {
-		$("nav").css("display", "inline-block")
-	} else {
-		$("nav").css("display", "none")
-	}
-}); 
 
-$(window).resize(function(){
-	if ($(window).width() < 580) {
-		$("ul").removeClass("flex")
-	} else {
-		$("ul").addClass("flex")
-	}
-});
-$('.one').on('click', function (e) {
+        (function() {
+
+            var navTrigger = $('.nav-trigger');
+
+            navTrigger.on('click', function () {
+
+                $('body').toggleClass('mobile-nav-open');
+
+            });
+
+        })();
+            
+
+ 
+
+
+    (function () {
+
+    $('nav a').not(document.getElementById('home'))
+
+        .on('click', function (e) {
+
         e.preventDefault();
+
+        var linkClass = $(this).attr('class');
+
         $('html, body').animate({
-            scrollTop: $('.about').offset().top + 'px'
+
+            scrollTop: $('.' + linkClass + '-section').offset().top + 'px'
+
         }, 300);
+
     });
-$('.two').on('click', function (e) {
-        e.preventDefault();
-        $('html, body').animate({
-            scrollTop: $('.services').offset().top + 'px'
-        }, 300);
-    });
-$('.three').on('click', function (e) {
-        e.preventDefault();
-        $('html, body').animate({
-            scrollTop: $('.gallery').offset().top + 'px'
-        }, 300);
-    });
-$('.four').on('click', function (e) {
-        e.preventDefault();
-        $('html, body').animate({
-            scrollTop: $('.blog').offset().top + 'px'
-        }, 300);
-    });
-$('.five').on('click', function (e) {
-        e.preventDefault();
-        $('html, body').animate({
-            scrollTop: $('.contact').offset().top + 'px'
-        }, 300);
-    });
+
+    })();
+
 })
 })(jQuery);
+
+
+
+window.onscroll = function() {myFunction()};
+  var header = document.getElementById("header");
+  var bg = header.offsetTop;
+
+  function myFunction(){
+    if (window.pageYOffset > bg) {
+      header.classList.add("bg-change");
+    } else {
+      header.classList.remove("bg-change");
+    }
+  };
